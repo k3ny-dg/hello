f14d7812ce8b6567b1d93eab00cc30bfa60a939a
Keny Dutton-Gillespie <dutton-gillespie.keny@student.greenriver.edu>
2022-04-12 13:43:45 -0400

    create a view route

--- index.php
+++ index.php
@@ -13,8 +13,11 @@ echo gettype($f3);
 
 // Define a default route
 $f3->route('GET /',function() {
-    echo '<h1>Hello, world!</h1>';
+
+    $view = new Template();
+    echo $view->render('views/home.html');
 }
+
 );
 
 // Run fat free
